91dd46a7e0a2cc8e059d00e634caefcb6b8e6aa5
Nnanyielugo <kz.onyegbule@gmail.com>
2018-04-19 18:53:18 +0100

    image resize feature

---
+++ api/controllers/upload.js
@@ -0,0 +1,50 @@
+import sharp from 'sharp';
+import request from 'request';
+import fs from 'fs';
+
+function generateRandomName(){
+  let characterBank = 'abcdefghijklmnopqrstuvwxyz1234567890';
+  let date = new Date().getTime();
+  let finishedString = 'file-';
+
+  for(let i = 0; i < 8; i++){
+    finishedString += characterBank[parseInt(Math.random()*26)]
+  } 
+  
+  return (finishedString += date)
+}
+
+function checkValidInput(input){
+  let response = false;
+  if(input.format === 'jpeg' || input.format === 'png' || input.format === 'jpg') {
+    response = true;
+  }
+
+  return response;
+}
+
+export const upload = (req, res, next) => {
+  const imageSrc = req.body.imageSrc;
+  request({url: imageSrc, encoding: null}, function(err, response, bodyBuffer){
+    sharp(bodyBuffer)
+      .resize(50, 50)
+      .toBuffer( (err, data, info) =>{
+        if(err) return next(err);
+
+        if(!checkValidInput(info)){
+          return res.status(400).json({error: "Invalid file input"})
+        }
+        // generate random filename and write image to filesystem
+        let generatedName = `./uploads/${generateRandomName()}.${info.format}`;
+        fs.writeFile(generatedName, data, function(err){if(err) next(err)});
+
+        // convert buffer to base64 string and send as json response
+        let base64Image = Buffer.from(data).toString('base64');
+        return res.json({data: base64Image, info: info})
+      })
+  })
+}
+
+
+
+
--- server.js
+++ server.js
@@ -1,10 +1,55 @@
 import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import methods from 'methods';
+import methodOverride from 'method-override';
+
+import api from './api/routes/index';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("Hello world")
+app.use(logger('dev'));
+app.use('/uploads', express.static('uploads'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride());
+app.use('/api', api);
+
+app.get('*', (req, res) => {
+  res.send("Hello fuckers!")
 })
+
+// error handlers
+app.use((req, res, next) => {
+  const error = new Error('not found');
+  error.status = 404;
+  next(error);
+});
+
+// development error handler
+if(!isProduction) {
+  app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(err.status || 500);
+    res.json({'errors': {
+      message: err.message,
+      error: err
+    }});
+  });
+}
+
+// production error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error : {
+      message: err.message
+    }
+  });
+});
 
 app.set('port', process.env.PORT || 5000);
 const server = app.listen(app.get('port'), () => {
